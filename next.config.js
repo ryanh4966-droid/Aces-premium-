0976f62b322cc429347392bff776279233b56c2a
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-06 08:11:49 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["m.media-amazon.com", "images-na.ssl-images-amazon.com"]
-  }
-};
-
-module.exports = nextConfig;
