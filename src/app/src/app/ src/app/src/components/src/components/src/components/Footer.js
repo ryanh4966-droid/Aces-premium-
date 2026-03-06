f77d9b66151db17b707fd0cf54fd8b96ee9e0d70
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-05 23:23:45 -0500

    Create Footer.js

---
+++ src/app/src/app/ src/app/src/components/src/components/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-8 text-center text-brand-white/60 mt-auto">
+      <p>© {new Date().getFullYear()} Aces Premium. All rights reserved.</p>
+    </footer>
+  );
+}
