6a48f738932b55d1c8fb73d91e9263e73e015ee0
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-05 23:15:37 -0500

    Add image domains to next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["m.media-amazon.com", "images-na.ssl-images-amazon.com"]
+  }
+};
+
+module.exports = nextConfig;
