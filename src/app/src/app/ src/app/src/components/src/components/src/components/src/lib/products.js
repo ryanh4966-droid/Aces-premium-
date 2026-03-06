11a15c11abee10f7e8faba2f721e2273b2118d8e
ryanh4966-droid <ryanh4966@gmail.com>
2026-03-05 23:25:35 -0500

    Create sampleProducts array in products.js
    Add sample product data for e-commerce display

---
+++ src/app/src/app/ src/app/src/components/src/components/src/components/src/lib/products.js
@@ -0,0 +1,26 @@
+export const sampleProducts = [
+  {
+    id: 1,
+    title: "Premium Noise‑Cancelling Headphones",
+    price: "$89.99",
+    image:
+      "https://m.media-amazon.com/images/I/61NwZKp0Q-L._AC_SL1500_.jpg",
+    link: "#"
+  },
+  {
+    id: 2,
+    title: "Smart LED Light Strip",
+    price: "$19.99",
+    image:
+      "https://m.media-amazon.com/images/I/71tHn1rVgCL._AC_SL1500_.jpg",
+    link: "#"
+  },
+  {
+    id: 3,
+    title: "Portable Mini Projector",
+    price: "$49.99",
+    image:
+      "https://m.media-amazon.com/images/I/71pN5Zt0VtL._AC_SL1500_.jpg",
+    link: "#"
+  }
+];
